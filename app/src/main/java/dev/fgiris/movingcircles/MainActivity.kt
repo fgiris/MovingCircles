44879e07fd9dbfdaa71ff9f65f91ba4fad25f08a
Fatih Giris <suleyman.fatih.giris@dnb.no>
2021-03-28 19:27:03 +0200

    Add text in the middle of circles

--- app/src/main/java/dev/fgiris/movingcircles/MainActivity.kt
+++ app/src/main/java/dev/fgiris/movingcircles/MainActivity.kt
@@ -3,16 +3,16 @@ package dev.fgiris.movingcircles
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.google.android.material.color.MaterialColors
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 import dev.fgiris.movingcircles.ui.theme.MovingCirclesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,15 +22,30 @@ class MainActivity : ComponentActivity() {
             MovingCirclesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MovingCircles(
+                    Box(
                         modifier = Modifier
+                            .fillMaxHeight()
                             .fillMaxWidth()
-                            .fillMaxHeight(),
-                        numberOfCircles = 32,
-                        distanceToCenter = 200f,
-                        circleColor = MaterialTheme.colors.onBackground,
-                        circleRadius = 10f
-                    )
+                    ) {
+                        MovingCircles(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(),
+                            numberOfCircles = 32,
+                            distanceToCenter = 200f,
+                            circleColor = MaterialTheme.colors.onBackground,
+                            circleRadius = 10f
+                        )
+
+
+                        Text(
+                            modifier = Modifier
+                                .align(Alignment.Center),
+                            text = "%50",
+                            color = MaterialTheme.colors.onBackground,
+                            fontSize = 32.sp
+                        )
+                    }
                 }
             }
         }
